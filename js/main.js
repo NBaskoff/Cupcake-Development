269d628bf062b24cf9917385e8adf63a72c24f5f
Baskoff <Baskoff@192.168.0.100>
2016-11-02 15:43:25 +0700

    end

--- js/main.js
+++ js/main.js
@@ -3,81 +3,78 @@ var basketBox;
 var basket = store.get('basket', []);
 var catalogData = new Object();
 
-window.onload = function () {
-    //Моделька главного меню
-    var menuTop = new Vue({
-        el: '#menuTop',
+//Моделька главного меню
+var menuTop = new Vue({
+    el: '#menuTop',
+    data: {
+        count: Object.keys(basket).length
+    },
+    methods: {
+        showCatalog: function () {
+            catalogBox.show = true;
+            basketBox.show = false;
+        },
+        showBasket: function () {
+            catalogBox.show = false;
+            basketBox.show = true;
+        },
+    }
+});
+
+var xhr = new XMLHttpRequest();
+var self = this;
+xhr.open('GET', "/catalog.json");
+xhr.onload = function () {
+    data = JSON.parse(xhr.responseText);
+    data.forEach(function (item, i, data) {
+        catalogData[item.id] = item;
+        catalogData[item.id].count = 1;
+    });
+    //Модель каталога
+    catalogBox = new Vue({
+        el: '#catalogBox',
         data: {
-            count: Object.keys(basket).length
+            catalog: catalogData,
+            show: true,
+            dialog: {title: "", image: ""}
         },
         methods: {
-            showCatalog: function () {
-                catalogBox.show = true;
-                basketBox.show = false;
+            addCard: function (item) {
+                basket[item.id] = item.count;
+                store.set('basket', basket);
+                basketBox.items[item.id] = item;
+                menuTop.count = Object.keys(basket).length;
             },
-            showBasket: function () {
-                catalogBox.show = false;
-                basketBox.show = true;
-            },
+            showInfo: function (item) {
+                this.dialog.title = item.title;
+                this.dialog.image = item.url;
+                $('#catalogInfo').modal('show');
+            }
         }
     });
 
-    var xhr = new XMLHttpRequest();
-    var self = this;
-    xhr.open('GET', "/catalog.json");
-    xhr.onload = function () {
-        data = JSON.parse(xhr.responseText);
-        data.forEach(function (item, i, data) {
-            catalogData[item.id] = item;
-            catalogData[item.id].count = 1;
-        });
-        //Модель каталога
-        catalogBox = new Vue({
-            el: '#catalogBox',
-            data: {
-                catalog: catalogData,
-                show: true,
-                dialog: {title: "", image: ""}
-            },
-            methods: {
-                addCard: function (item) {
-                    basket[item.id] = item.count;
-                    store.set('basket', basket);
-                    basketBox.items[item.id] = item;
-                    menuTop.count = Object.keys(basket).length;
-                },
-                showInfo: function (item) {
-                    this.dialog.title = item.title;
-                    this.dialog.image = item.url;
-                    $('#catalogInfo').modal('show');
-                }
+    //Сфомируем данные для коризны
+    basketData = new Object();
+    for (item in basket) {
+        basketData[item] = catalogData[item];
+    }
+
+    //модель корзины
+    basketBox = new Vue({
+        el: '#basketBox',
+        data: {
+            items: basketData,
+            show: false,
+        },
+        methods: {
+            sendCard: function () {
+                $('#basketInfo').modal('show');
+                basket = new Object();
+                store.set('basket', basket);
+                this.items = basket;
+                menuTop.count = Object.keys(basket).length;
             }
-        });
-
-        //Сфомируем данные для коризны
-        basketData = new Object();
-        jQuery.each(basket, function (item, key) {
-            basketData[item] = catalogData[item];
-        });
-
-        //модель корзины
-        basketBox = new Vue({
-            el: '#basketBox',
-            data: {
-                items: basketData,
-                show: false,
-            },
-            methods: {
-                sendCard: function () {
-                    $('#basketInfo').modal('show');
-                    basket = new Object();
-                    store.set('basket', basket);
-                    this.items = basket;
-                    menuTop.count = Object.keys(basket).length;
-                }
-            }
-        });
-    };
-    xhr.send();
-
-}
+        }
+    });
+};
+xhr.send();
